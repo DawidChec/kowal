c1bc028e077a2eae57ba220b46551de68c91f3f2
Dawid Chec <checdd@gmail.com>
2024-02-13 09:49:30 +0100

    Step 2

--- main.py
+++ main.py
@@ -3,15 +3,30 @@ import random
 def item(x):
     return x
 
-level_item = int(input("Type in item lvl. "))
+level_item = int(input("Type in item lvl: "))
 
 while True:
-    user_upgrades = input("Uppgrade your item? (+/-) ")
+    user_upgrades = input("Uppgrade your item? (+/-): ")
     if user_upgrades == "+":
-        upgraded_item = item(level_item) + random.randint(1, 3)
-        level_item = upgraded_item
-        print(upgraded_item)
+        if level_item <= 11:
+            upgraded_item = item(level_item) + random.randint(1, 3)
+            level_item = upgraded_item
+            print(upgraded_item)
 
-    user_still_upgrades = input("Do you wish to still upgrade? (Y/n): ").lower()
-    if user_still_upgrades != "y":
+        elif level_item == 12:
+            chance = random.randint(1, 100)
+            if chance <= 1:
+                upgraded_item = item(level_item) + random.randint(1, 3)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(burned_item)
+
+
+
+
+    continue_upgrades = input("Do you wish to still upgrade? (Y/n): ").lower()
+    if continue_upgrades != "y":
         break
