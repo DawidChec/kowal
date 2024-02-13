6d667ad9049e52665c889ebcf72fefc743b421ce
Dawid Chec <checdd@gmail.com>
2024-02-13 10:01:52 +0100

    Step 2

--- main.py
+++ main.py
@@ -15,6 +15,94 @@ while True:
 
         elif level_item == 12:
             chance = random.randint(1, 100)
+            if chance <= 40:
+                upgraded_item = item(level_item) + random.randint(1, 3)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 1)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
+
+        elif level_item == 13:
+            chance = random.randint(1, 100)
+            if chance <= 50:
+                upgraded_item = item(level_item) + random.randint(1, 3)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
+
+        elif level_item == 14:
+            chance = random.randint(1, 100)
+            if chance <= 30:
+                upgraded_item = item(level_item) + random.randint(1, 3)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
+
+        elif level_item == 15:
+            chance = random.randint(1, 100)
+            if chance <= 24:
+                upgraded_item = item(level_item) + random.randint(1, 3)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
+
+        elif level_item == 16:
+            chance = random.randint(1, 100)
+            if chance <= 10:
+                upgraded_item = item(level_item) + random.randint(1, 3)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
+
+        elif level_item == 17:
+            chance = random.randint(1, 100)
+            if chance <= 14:
+                upgraded_item = item(level_item) + random.randint(1, 3)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
+
+        elif level_item == 18:
+            chance = random.randint(1, 100)
+            if chance <= 8:
+                upgraded_item = item(level_item) + random.randint(1, 3)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
+
+        elif level_item == 19:
+            chance = random.randint(1, 100)
+            if chance <= 2:
+                upgraded_item = item(level_item) + random.randint(1, 3)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
+
+        elif level_item == 20:
+            chance = random.randint(1, 100)
             if chance <= 1:
                 upgraded_item = item(level_item) + random.randint(1, 3)
                 level_item = upgraded_item
@@ -22,8 +110,40 @@ while True:
             else:
                 burned_item = item(level_item) - random.randint(0, 2)
                 level_item = burned_item
-                print(burned_item)
+                print(f"Your item has burned to + {burned_item}")
 
+        elif level_item == 21:
+            chance = random.randint(1, 100)
+            if chance <= 3:
+                upgraded_item = item(level_item) + random.randint(1, 3)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
+
+        elif level_item == 22:
+            chance = random.randint(1, 100)
+            if chance <= 3:
+                upgraded_item = item(level_item) + random.randint(1, 2)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
+
+        elif level_item == 23:
+            chance = random.randint(1, 100)
+            if chance <= 4:
+                upgraded_item = item(level_item) + random.randint(1, 1)
+                level_item = upgraded_item
+                print(upgraded_item)
+            else:
+                burned_item = item(level_item) - random.randint(0, 2)
+                level_item = burned_item
+                print(f"Your item has burned to + {burned_item}")
 
 
 
